d22962ab036929dc01f51b81f83dec7eeba967a6
Luke Fisher <mail@lukefisher.me>
2017-10-15 04:57:16 +0100

    Add in basic code for visitor controller list/add/get

--- src/Controller/VisitorController.php
+++ src/Controller/VisitorController.php
@@ -7,6 +7,31 @@ use Psr\Http\Message\ResponseInterface as Response;
 
 class VisitorController extends Controller
 {
+    public function list(Request $request, Response $response) :Response {
+        $user = $request->getAttribute('user');
+
+        if (!isset($user)) {
+            return $response->withStatus(401);
+        }
+
+        $database = $this->container->get('database');
+        $visitors = $database->table('visitor')
+            ->where([
+                ['user_id', '=', $user->id]
+            ])
+            ->select('id', 'name')
+            ->get();
+
+        if ($visitors != null) {
+            return $response->withHeader('Content-Type', 'application/json'))
+                ->withBody(json_encode([
+                    'visitors' => $visitors
+                ]));
+        } else {
+            return $response->withStatus(404);
+        }
+    }
+
     // /visitor/{id}
     public function get(Request $request, Response $response, $args): Response
     {
@@ -24,13 +49,12 @@ class VisitorController extends Controller
                 ['id', '=', $visitor_id]
                 ['user_id', '=', $user->id]
             ])
+            ->select('id', 'name')
             ->first();
 
         if ($visitor != null) {
             return $response->withHeader('Content-Type', 'application/json')
-                ->withBody(json_encode([
-                    '' =>
-                ]));
+                ->withBody(json_encode($visitor));
         } else {
             return $response->withStatus(404);
         }
@@ -47,7 +71,20 @@ class VisitorController extends Controller
 
         $data = $request->getParsedBody();
 
+        $database = $this->container->get('database');
+        $insertResult = $database->table('visitor')
+            ->insert([
+                [
+                    'name' => $data->name,
+                    'user_id' => $user->id,
+                ]
+            ]);
 
+        if ($insertResult) {
+            return $response;
+        } else {
+            return $response->withStatus(400);
+        }
     }
 
     // /visitor/{id}
